271b511d4b50e0489b690f08b39090769ab2b4fd
BuildTools <unconfigured@null.spigotmc.org>
2021-03-17 15:29:18 +0100

    refactoring, preventing user from setting negative watering intervals

--- Antworten.php
+++ Antworten.php
@@ -1,83 +1,84 @@
 <?php
 $plantFacts = [
-	"„Fette Henne“ – das ist kein sehr dickes Huhn, sondern eine Pflanze. Die Fette Henne kann mit ihren dicken Blättern auch gut ohne Wasser auskommen.",
+    "„Fette Henne“ – das ist kein sehr dickes Huhn, sondern eine Pflanze. Die Fette Henne kann mit ihren dicken Blättern auch gut ohne Wasser auskommen.",
 
-	"„Wolfsmilch“ diese Pflanze hat überhaupt nichts mit einem Wolf zu tun, aber dafür mit Milch. Bei dem „Milchsaft“ handelt es sich um giftigen Saft, der bei Berührung der Haut allergische Reaktionen hervorrufen kann.",
+    "„Wolfsmilch“ diese Pflanze hat überhaupt nichts mit einem Wolf zu tun, aber dafür mit Milch. Bei dem „Milchsaft“ handelt es sich um giftigen Saft, der bei Berührung der Haut allergische Reaktionen hervorrufen kann.",
 
-	"Es gitb einen „Leberwurstbaum“ - ja richtig gelesen! Es gibt tatsächlich einen Leberwurstbaum! Der ist in Afrika in der Umgebung rund um den Fluss Luangwa zu finden. Er ist dort für die Tiere lebensnotwendig, weil er Nahrung und Schutz bietet.",
+    "Es gitb einen „Leberwurstbaum“ - ja richtig gelesen! Es gibt tatsächlich einen Leberwurstbaum! Der ist in Afrika in der Umgebung rund um den Fluss Luangwa zu finden. Er ist dort für die Tiere lebensnotwendig, weil er Nahrung und Schutz bietet.",
 
-	"Wenn Bäume in der Nähe von Straßenlaternen stehen, verlieren sie nicht so viele Blätter",
+    "Wenn Bäume in der Nähe von Straßenlaternen stehen, verlieren sie nicht so viele Blätter",
 
-	"Aus Gänseblümchen kann man eine Salbe zur Minderung von Sommersprossen herstellen.",
+    "Aus Gänseblümchen kann man eine Salbe zur Minderung von Sommersprossen herstellen.",
 
-	"Bestimmte Bambusarten können innerhalb von 24 Stunden 91 cm wachsen.",
+    "Bestimmte Bambusarten können innerhalb von 24 Stunden 91 cm wachsen.",
 
-	"Der „Küstenmammutbaum“ steht in Kalifornien und ist 115,92 m hoch. Er gilt als der höchste noch am lebende Baum der Welt.",
+    "Der „Küstenmammutbaum“ steht in Kalifornien und ist 115,92 m hoch. Er gilt als der höchste noch am lebende Baum der Welt.",
 
-	"Das größte Blatt der Welt kommt aus Taiwan und hat einen Durchmesser von drei Metern. Das Riesenblatt hält ca. 80 kg aus. Man kann sich entspannt draufsetzten, ohne nass zu werden.",
+    "Das größte Blatt der Welt kommt aus Taiwan und hat einen Durchmesser von drei Metern. Das Riesenblatt hält ca. 80 kg aus. Man kann sich entspannt draufsetzten, ohne nass zu werden.",
 
-	"Botanisch betrachtet gehört die Erdnuss zur Familie der Hülsenfrüchtler (wie auch Erbsen und Bohnen).
+    "Botanisch betrachtet gehört die Erdnuss zur Familie der Hülsenfrüchtler (wie auch Erbsen und Bohnen).
 Die Erdnuss wächst unterirdisch, und dies hat der Frucht den Namen „Erdnuss“ eingebracht.",
 
-	"Hyperion ist der Name eines Küstenmammutbaums (Sequoia sempervirens) in Kalifornien, der mit 115,92 m als der längste bekannte lebende Baum der Welt gilt.
+    "Hyperion ist der Name eines Küstenmammutbaums (Sequoia sempervirens) in Kalifornien, der mit 115,92 m als der längste bekannte lebende Baum der Welt gilt.
 Hyperion wurde am 25. August 2006 von den Naturforschern Chris Atkins und Michael Taylor entdeckt. Der Baum steht in einem abgelegenen Gebiet des Redwood-Nationalpark (Kalifornien). Er enthält schätzungsweise 530 Kubikmeter Holz und sein Alter wird auf 600 bis 800 Jahre geschätzt. ",
 
-	"Blumen haben nicht immer existiert. Die ersten Blumen werden vor 130 Millionen Jahren vermutet. Die Amborella ist wahrscheinlich der Vorfahre der heutigen bekannten Pflanzen.",
+    "Blumen haben nicht immer existiert. Die ersten Blumen werden vor 130 Millionen Jahren vermutet. Die Amborella ist wahrscheinlich der Vorfahre der heutigen bekannten Pflanzen.",
 
-	"Tulpenzwiebeln waren im 17. Jahrhundert mehr wert als Gold. In dieser Zeit ist der Wert von Tulpen so stark angestiegen, dass sie sogar als Währung genutzt wurden. Was ziemlich merkwürdig ist, wenn man bedenkt, dass die Haltbarkeit einer Tulpe bei nur etwa 1 Woche liegt.",
+    "Tulpenzwiebeln waren im 17. Jahrhundert mehr wert als Gold. In dieser Zeit ist der Wert von Tulpen so stark angestiegen, dass sie sogar als Währung genutzt wurden. Was ziemlich merkwürdig ist, wenn man bedenkt, dass die Haltbarkeit einer Tulpe bei nur etwa 1 Woche liegt.",
 
-	"Die Titanwurz ist die größte Blume der Welt. Leider ist sie auch bekannt für ihren fischigen Geruch, weshalb sie im Englischen auch den  Namen Corpse Flower (Leichnam Blume) trägt.  Die stinkende Blume ist etwa 2,50 m hoch und die Blüte hat einen Durchmesser von bis zu 3,70 m.",
+    "Die Titanwurz ist die größte Blume der Welt. Leider ist sie auch bekannt für ihren fischigen Geruch, weshalb sie im Englischen auch den Namen Corpse Flower (Leichnam Blume) trägt. Die stinkende Blume ist etwa 2,50 m hoch und die Blüte hat einen Durchmesser von bis zu 3,70 m.",
 
-	"Es gibt viele Blumen, die wunderbar riechen. Wie die Schokoladen-Kosmee. Die bordeauxrote Blume hat den süßen und unverkennbaren Geruch von Zartbitterschokolade.",
+    "Es gibt viele Blumen, die wunderbar riechen. Wie die Schokoladen-Kosmee. Die bordeauxrote Blume hat den süßen und unverkennbaren Geruch von Zartbitterschokolade.",
 
-	"Hast du Schlafprobleme? Eine Vase mit Gerbera könnte helfen. Die Blume setzt nämlich in der Nacht Sauerstoff frei und erleichtert das Atmen. Besonders bei Allergiebeschwerden kann die Gerbera für einen besseren Schlaf sorgen.",
+    "Hast du Schlafprobleme? Eine Vase mit Gerbera könnte helfen. Die Blume setzt nämlich in der Nacht Sauerstoff frei und erleichtert das Atmen. Besonders bei Allergiebeschwerden kann die Gerbera für einen besseren Schlaf sorgen.",
 
-	"Hast du dich schon mal gefragt, wie die Verdauung bei  Fleischfressende Pflanzen funktioniert?  Die Venusfliegenfalle nutzt zum Beispiel die kleinen Haare auf ihren Blättern, um Insekten zu fangen. Wenn ein Insekt mit diesen Haaren in Kontakt kommt, schlägt die Falle zu und die Pflanze zersetzt ihre Beute anschließend mit Sekreten, die die Insekten auflösen und die Nährstoffe auslösen.",
+    "Hast du dich schon mal gefragt, wie die Verdauung bei  Fleischfressende Pflanzen funktioniert?  Die Venusfliegenfalle nutzt zum Beispiel die kleinen Haare auf ihren Blättern, um Insekten zu fangen. Wenn ein Insekt mit diesen Haaren in Kontakt kommt, schlägt die Falle zu und die Pflanze zersetzt ihre Beute anschließend mit Sekreten, die die Insekten auflösen und die Nährstoffe auslösen.",
 
-	"Die Agave fruchtet nur ein mal in ihrem Leben. Sie wächst für viele Jahre ohne Blüten zu produzieren und kurz nachdem ihre seltene und bis zu 12 Meter hohe Blüte gewachsen ist, stirbt die Pflanze ab.",
+    "Die Agave fruchtet nur ein mal in ihrem Leben. Sie wächst für viele Jahre ohne Blüten zu produzieren und kurz nachdem ihre seltene und bis zu 12 Meter hohe Blüte gewachsen ist, stirbt die Pflanze ab.",
 
-	"Die Ipomoea alba heißt Mondblume, weil sie sich in der Nacht öffnet und während des Tages geschlossen ist.",
+    "Die Ipomoea alba heißt Mondblume, weil sie sich in der Nacht öffnet und während des Tages geschlossen ist.",
 
-	"Die Distel ist Schottlands Nationalblume. Denn dank der großflächig gewachsenen Distel wurden früher die Wikinger bei ihren Schlachtzügen in Schottland aufgehalten. So hatten die Einheimischen genug Zeit, um sich in Sicherheit zu bringen.",
+    "Die Distel ist Schottlands Nationalblume. Denn dank der großflächig gewachsenen Distel wurden früher die Wikinger bei ihren Schlachtzügen in Schottland aufgehalten. So hatten die Einheimischen genug Zeit, um sich in Sicherheit zu bringen.",
 
-	"Die Farbe von Hortensien wird vom Säuregehalt der Erde bestimmt. Wenn die Erde sehr alkalisch ist, wird die Blüte pink. Ist der Boden dagegen eher sauer, produziert die Hortensie blaue oder lila Blüten.",
+    "Die Farbe von Hortensien wird vom Säuregehalt der Erde bestimmt. Wenn die Erde sehr alkalisch ist, wird die Blüte pink. Ist der Boden dagegen eher sauer, produziert die Hortensie blaue oder lila Blüten.",
 
-	"Einige Tatsachen über Blumen sind, dass Blumen ihre Nahrung von Sonnenlicht, Wasser und Mineralien im Boden bekommen; Blumen sind die reproduktiven Teile einer Pflanze, und Pflanzen machen ihre eigene Nahrung durch Photosynthese.",
+    "Einige Tatsachen über Blumen sind, dass Blumen ihre Nahrung von Sonnenlicht, Wasser und Mineralien im Boden bekommen; Blumen sind die reproduktiven Teile einer Pflanze, und Pflanzen machen ihre eigene Nahrung durch Photosynthese.",
 
-	"Ohne Sonnenlicht und Wasser werden Pflanzen sterben.",
+    "Ohne Sonnenlicht und Wasser werden Pflanzen sterben.",
 
-	"Die Farbe von Hortensien wird vom Säuregehalt der Erde bestimmt. Wenn die Erde sehr alkalisch ist, wird die Blüte pink. Ist der Boden dagegen eher sauer, produziert die Hortensie blaue oder lila Blüten.",
+    "Die Farbe von Hortensien wird vom Säuregehalt der Erde bestimmt. Wenn die Erde sehr alkalisch ist, wird die Blüte pink. Ist der Boden dagegen eher sauer, produziert die Hortensie blaue oder lila Blüten.",
 
-	"Brokkoli ist eigentlich eine Blume.",
+    "Brokkoli ist eigentlich eine Blume.",
 
-	"Einige Pflanzen wie Orchideen brauchen keine Erde, um zu wachsen – sie bekommen alle ihre Nährstoffe aus der Luft.",
+    "Einige Pflanzen wie Orchideen brauchen keine Erde, um zu wachsen – sie bekommen alle ihre Nährstoffe aus der Luft.",
 
-	"Einige Pflanzen produzieren giftige Substanzen, die andere Pflanzen um sie herum töten – die Sonnenblume ist ein Beispiel dafür.",
+    "Einige Pflanzen produzieren giftige Substanzen, die andere Pflanzen um sie herum töten – die Sonnenblume ist ein Beispiel dafür.",
 
-	"Mimosa Punica, oder empfindliche Pflanze, wird ihre Blätter zusammenfalten, wenn sie berührt wird. Sie hat weißlich rosa flaumige Blumen, die wie kleine Pompons aussehen.",
+    "Mimosa Punica, oder empfindliche Pflanze, wird ihre Blätter zusammenfalten, wenn sie berührt wird. Sie hat weißlich rosa flaumige Blumen, die wie kleine Pompons aussehen.",
 
 ];
 
 $wateringPraises = [
-	"Gute Arbeit!",
-	"#Ehrengärtner!",
-	"Du bist ein wahrer Pflanzenfreund!",
-	"Ich bin stolz auf dich!",
-	"Gut gemacht!",
-	"Sauber!",
-	"Nice gemacht vong Flüssigkeitszufuhr her!",
-	"Richtig knorke!",
-	"Die Firma dankt!",
-	"Eine feuchte Pflanze ist eine glückliche Pflanze!",
-	"Episch!",
-	"Majestätisch!",
-	"Du machst die Welt zu einem besseren Ort!",
-	"Beeindruckend!",
-	"Das hast du gut gemacht!",
-	"Nice!",
-	"Sonnenlicht und Wasser gegen Sauerstoff - guter Tausch!"
+    "Gute Arbeit!",
+    "#Ehrengärtner!",
+    "Du bist ein wahrer Pflanzenfreund!",
+    "Ich bin stolz auf dich!",
+    "Gut gemacht!",
+    "Sauber!",
+    "Nice gemacht vong Flüssigkeitszufuhr her!",
+    "Richtig knorke!",
+    "Die Firma dankt!",
+    "Eine feuchte Pflanze ist eine glückliche Pflanze!",
+    "Episch!",
+    "Majestätisch!",
+    "Du machst die Welt zu einem besseren Ort!",
+    "Beeindruckend!",
+    "Das hast du gut gemacht!",
+    "Nice!",
+    "Sonnenlicht und Wasser gegen Sauerstoff - guter Tausch!"
 ];
 
-function randomReplyFromArray ($array) {
-	return $array[array_rand($array)];
+function randomReplyFromArray($array)
+{
+    return $array[array_rand($array)];
 }
